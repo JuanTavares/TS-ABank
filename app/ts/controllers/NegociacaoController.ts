9a05d9cff09cffe06f7039dcd39254743a558836
Juan Carlos <juantavares2006@hotmail.com>
2018-07-21 12:27:26 -0300

    Método adiciona

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -10,4 +10,16 @@ class NegociacaoController {
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
     }
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value);
+
+        console.log(negociacao);
+    }
 }
