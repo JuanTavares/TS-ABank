f76eecc9a82a8e0e17a54205f77bff9e8d63ea0e
Juan Tavares <juantavares2006@hotmail.com>
2018-10-04 21:34:43 -0300

    Importando e aplicando a interface Igualavel

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,12 +1,21 @@
-export class Negociacao {
+import { Igualavel } from './Igualavel';
+
+export class Negociacao implements Igualavel<Negociacao>{
 
     constructor(
-        readonly data: Date, 
-        readonly quantidade: number,  
-        readonly valor: number) {}
+        readonly data: Date,
+        readonly quantidade: number,
+        readonly valor: number) { }
 
 
     get volume() {
         return this.quantidade * this.valor;
+    }
+
+    ehIgual(negociacao: Negociacao): boolean {
+
+        return this.data.getDate() == negociacao.data.getDate()
+            && this.data.getMonth() == negociacao.data.getMonth()
+            && this.data.getFullYear() == negociacao.data.getFullYear();
     }
 }
